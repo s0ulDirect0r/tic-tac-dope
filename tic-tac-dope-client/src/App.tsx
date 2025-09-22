133ceecbd45ba641502c6456c23b62552d71690a
Matthew Huff <matthewhuff89@gmail.com>
2025-09-22 11:05:43 -0400

    install vite and create grid

---
+++ tic-tac-dope-client/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='game-grid'>
+        <div>X</div>
+        <div>O</div>
+        <div>X</div>
+        <div>O</div>
+        <div>X</div>
+        <div>O</div>
+        <div>X</div>
+        <div>O</div>
+        <div>X</div>
+      </div>
+    </>
+  )
+}
+
+export default App
