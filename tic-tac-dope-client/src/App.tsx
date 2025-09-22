fc80c8f08ac7f9f06c20a2c2dbb51362405a809f
Matthew Huff <matthewhuff89@gmail.com>
2025-09-22 15:47:48 -0400

    Game logic working properly, end state styling

--- tic-tac-dope-client/src/App.tsx
+++ tic-tac-dope-client/src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react"
-import { initialGameState, makeMove } from "./tictacdope"
+import { initialGameState, makeMove, type GameState } from "./tictacdope"
 
 function App() {
   const [gameState, setGameState] = useState(initialGameState)
 
   const handleClick = ({ row, column }: { row: number, column: number }) => {
+    if(gameState.winner) {
+        return true
+    }
     console.log(gameState)
     const newGameState = makeMove(gameState, row, column)
     setGameState(newGameState)
+  }
+
+  const handleReset = () => {
+    setGameState({
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ],
+      winner: null,
+      currentPlayer: "X"
+    })
   }
 
   const board = gameState.board
@@ -18,6 +33,12 @@ function App() {
         <div className="m-11 mt-20 mb-20">
           <h1 className="text-center text-white font-bold text-7xl">Tic-Tac-Dope</h1>
         </div>
+        {gameState.winner && 
+          <div className="flex flex-col items-center">
+            <h1 className="text-center mb-10 font-bold text-5xl text-white">GAME OVER {gameState.winner} WINS!!</h1>
+            <button className="bg-green-800 mb-10 p-10 text-white font-bold" onClick={handleReset}>PLAY AGAIN</button>
+          </div>   
+        }
         <div className='m-auto content-center grid grid-cols-3 grid-rows-3 gap-3'>
             <div className="bg-green-500 p-8 text-7xl text-white font-bold text-center" onClick={() => handleClick({row: 0, column: 0})}>{board[0][0]}</div>
             <div className="bg-green-500 p-8 text-7xl text-white font-bold text-center" onClick={() => handleClick({row: 0, column: 1})}>{board[0][1]}</div>
--- tic-tac-dope-client/src/tictacdope.ts
+++ tic-tac-dope-client/src/tictacdope.ts
@@ -2,7 +2,7 @@
 type Player = "X" | "O"
 type Cell = Player | null
 
-interface GameState {
+export interface GameState {
   board: Cell[][] | undefined[][],
   currentPlayer: Player | null,
   winner: Player | null
@@ -18,6 +18,39 @@ export const initialGameState: GameState = {
   winner: null
 }
 
+// Check if a player has won the game
+export const isWinner = (board: Cell[][] | undefined[][], player: Player) => {
+  // is there a smarter way than preprogramming conditions?
+  // let's just do it dumb first
+  // X Win States
+  if(!player) {
+    return null
+  }
+  // check the rows
+  if (board[0].every(cell => cell === player) || board[1].every(cell => cell === player) || board[2].every(cell => cell === player)) {
+    return player
+  }
+
+  // consolidate columns
+  const columnOne = [board[0][0], board[1][0], board[2][0]]
+  const columnTwo = [board[0][1], board[1][1], board[2][1]]
+  const columnThree = [board[0][2], board[1][2], board[2][2]]
+
+  if (columnOne.every(cell => cell === player) || columnTwo.every(cell => cell === player) || columnThree.every(cell => cell === player)) {
+    return player
+  }
+
+  // check diaganols
+  const diagonalOne = [board[0][0], board[1][1], board[2][2]]
+  const diagonalTwo = [board[2][0], board[1][1], board[0][2]]
+  // O Win State
+  if(diagonalOne.every(cell => cell === player) || diagonalTwo.every(cell => cell === player)) {
+    return player
+  }
+
+  return null
+}
+
 // When a user clicks on a square
 // The game state is updated, so that the cell 
 // makeMove(gameState, "X", )
@@ -29,10 +62,16 @@ export const makeMove = (gameState: GameState, row: number, column: number): Gam
   }
   console.log(gameState.currentPlayer)
   gameStateCopy.board[row][column] = gameState.currentPlayer
+  if (gameStateCopy.currentPlayer) {
+    const winner = isWinner(gameStateCopy.board, gameStateCopy.currentPlayer)
+    gameStateCopy.winner = winner
+  }
+
   if(gameStateCopy.currentPlayer === "X") {
     gameStateCopy.currentPlayer = "O"
   } else if(gameStateCopy.currentPlayer === "O") {
     gameStateCopy.currentPlayer = "X"
   }
+
   return gameStateCopy
 }
